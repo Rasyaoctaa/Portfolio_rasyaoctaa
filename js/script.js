4c9471dc4b0a0846bd15e27e4e882d8a29a0248c
Rasyaoctaa <93633139+Rasyaoctaa@users.noreply.github.com>
2021-11-03 10:59:08 +0700

    Upload Web Portfolio
    Mengupload web portfolio saya!

---
+++ js/script.js
@@ -0,0 +1,48 @@
+/* Efek Ngetik */
+
+const txtElement = ['siswi smk muthia harapan cicalengka'];
+let count = 0;
+let txtIndex = 0;
+let currentTxt = '';
+let words = '';
+
+(function ngetik(){
+
+	if(count == txtElement.length){
+		count = 0;
+	}
+
+	currentTxt = txtElement[count];
+
+	words = currentTxt.slice(0, ++txtIndex);
+	document.querySelector('.efek-ngetik').textContent = words;
+
+	if(words.length == currentTxt.length){
+		count++;
+		txtIndex = 0;
+	}
+
+	setTimeout(ngetik, 500);
+
+})();
+
+/* Tombol Scroll Ke Atas */
+
+mybutton = document.getElementById("top-btn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
